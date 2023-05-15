494df1b229b19d7afeaaee4c12d5331b931fa505
agkosd <sonawaneakshay039@gmail.com>
2023-05-15 23:37:23 +1000

    Intermediate commits to save the current scraping state

--- spider_tutorial/spider_tutorial/spiders/woldometer.py
+++ spider_tutorial/spider_tutorial/spiders/woldometer.py
@@ -1,19 +1,35 @@
 import scrapy
 
+
 class WoldometerSpider(scrapy.Spider):
     name = "woldometer"
     allowed_domains = ["www.worldometers.info"]
-    start_urls = ["https://www.worldometers.info/world-population/population-by-country"]
+    start_urls = [
+        "https://www.worldometers.info/world-population/population-by-country"]
 
     def parse(self, response):
         country_links = response.xpath('//td/a')
 
-        list = []
-        for link in country_links:
-            country_name = link.xpath('.//text()').get()
-            link = link.xpath('.//@href').get()
-            list.append({'country_name':country_name, 'link':response.urljoin(link)})
-            
-        yield{
-            'country-links':list
+        for country in country_links[1:2]:
+            country_name = country_links.xpath(".//text()").get()
+            link = country.xpath('.//@href').get()
+
+            yield response.follow(url=link, callback=self.parse_country, meta={'country': country_name})
+
+    def parse_country(self, response):
+        country = response.request.meta
+        rows = response.xpath(
+            "(//table[contains(@class, 'table')])[1]/tbody/tr")
+        _response[str(country)] = []
+
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+            _response[str(country)].append(
+                {'year': year, 'population': population})
+
+        yield {
+            [country]: {
+
+            }
         }
